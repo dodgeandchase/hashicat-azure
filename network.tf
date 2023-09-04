cb20d05463ecd8bd25dbb611a8672d1990e0ccb1
edmante <edmante@example.com>
2023-09-04 13:51:06 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,8 +1,14 @@
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "West Europe"
+}
+
+
 
 module "network" {
   source  = "app.terraform.io/edmante/network/azurerm"
   version = "3.5.0"
-  # insert required variables here
-  resource_group_name = mine
+  # insert required variables h
+  resource_group_name = azurerm_resource_group.example.name
 }
 
