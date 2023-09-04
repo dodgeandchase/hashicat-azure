b38109bba32f3a10cd139e2aa505a7ae6873d794
edmante <edmante@example.com>
2023-09-04 13:47:23 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,19 +1,7 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "West Europe"
-}
 
 module "network" {
   source  = "app.terraform.io/edmante/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.example.name
-  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 }
 
